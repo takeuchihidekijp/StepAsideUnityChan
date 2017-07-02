d3ccc10dd526b154f3fbbedaf60e60391bd5f34f
hideki takeuchi <takeuchihidekijp@yahoo.co.jp>
2017-07-03 01:08:54 +0900

    通常課題

---
+++ Assets/MyDestance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyDestance : MonoBehaviour {
+
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+
+
+    // Use this for initialization
+    void Start () {
+
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (unitychan.transform.position.z > this.transform.position.z)
+        {
+            Destroy(this);
+        }
+		
+	}
+}
